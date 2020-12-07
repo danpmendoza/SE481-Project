d2e5568507e05e2edd24c60f49cb093f34332a5e
DESKTOP-KD6EEGF\DanMe <mendo263@cougars.csusm.edu>
2020-12-06 20:16:03 -0800

    add maven

---
+++ ciprojecttest/src/main/java/BasicMath.java
@@ -0,0 +1,24 @@
+public class BasicMath{
+	
+	// from dev
+	public double add(double a, double b){
+		
+		double c;
+		c = a + b;
+
+		return c;
+	}
+
+	public double returnLessNum( double a, double b){
+
+		double c;
+
+		if(a < b){
+			c = a;
+		}
+		else{
+			c = b;
+		}
+		return c;
+	}
+}
